6d5afcabd01e71381ae2d1ac7ea53d9fbaac8861
Beatriz <beatriz.216@hotmail.com>
2012-12-12 15:53:34 -0200

    Tentativa de classes 'DAO'
    Falta terminar.

--- Codigo/persistence/FactoryConnection.java
+++ Codigo/persistence/FactoryConnection.java
@@ -1,13 +1,23 @@
 package persistence;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 //TODO conexao com o MySQL
 public class FactoryConnection {
-
+		static String statusConnection = "";
+		
 	public static Connection getConnection() {
-		return null;
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost/teste?user=matheus&password=1234");
+			System.out.println("OK!");	
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return con;
 	}
 
 }
--- Codigo/persistence/ProfessorDAO.java
+++ Codigo/persistence/ProfessorDAO.java
@@ -2,7 +2,7 @@ package persistence;
 
 import model.Professor;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.util.Vector;
 
 public class ProfessorDAO {
@@ -21,19 +21,79 @@ public class ProfessorDAO {
 	//
 	
 	public void incluir(Professor prof) {
-		//TODO
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		
+		try {
+			pst = con.prepareStatement("INSERT INTO cliente(nome, cpf, telefone, email, matricula) " +
+					"VALUES (\"" + prof.getNome() + 
+							"\", \"" + prof.getCpf()+ "\"," +
+							" \"" + prof.getTelefone() +
+							"\", \"" + prof.getEmail() +
+							"\", \"" + prof.getMatricula() +
+					"\"");
+			rs = pst.executeQuery();
+			
+			pst = con.prepareStatement("INSERT INTO professor(id_cliente) (SELECT id_cliente From cliente WHERE " +
+					"cliente.nome = \"" + prof.getNome() + 
+							"\" and cliente.cpf = \"" + prof.getCpf()+ "\" " +
+							" \" and cliente.telefone = " + prof.getTelefone() +
+							"\" and cliente.email = \"" + prof.getEmail() +
+							"\"and cliente.matricula = \"" + prof.getMatricula() +
+					"\")");
+			rs = pst.executeQuery();
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 	public void alterar(Professor prof) {
-		//TODO
+		
+		Statement st = null;
+				
+		try {
+			st = con.createStatement();
+			st.executeUpdate("UPDATE cliente SET nome=\"" + prof.getNome() + 
+					"\", cpf=\"" + prof.getCpf() +
+					"\", telefone= \"" + prof.getTelefone() +
+					"\", email=\"" + prof.getEmail() +
+					"\", matricula=\"" + prof.getMatricula() + "\""+
+					
+					"WHERE nome=\"" + prof.getNome() + 
+					"\"and cpf=\"" + prof.getCpf() +
+					"\"");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public void excluir(Professor prof) {
-		//TODO
+		
 	}
 
-	public Professor buscar() {
-		//TODO
+	public Professor buscar(Professor prof) {
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		
+		try {
+			pst = con.prepareStatement("INSERT INTO cliente(nome, cpf, telefone, email, matricula) " +
+					"VALUES (\""+ prof.getCpf()+ "\"," +
+							" \"" + prof.getTelefone() +
+							"\", \"" + prof.getEmail() +
+							"\", \"" + prof.getMatricula() +
+					"\"");
+			rs = pst.executeQuery();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
