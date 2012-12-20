286f48ecffe21736c5fba12c1046bd9fffd4c2b4
Matheus Faria <matheus.sousa.faria@gmail.com>
2012-12-19 22:35:12 -0200

    Inserindo novas infos na Arquitetura

--- Codigo/control/ManterSala.java
+++ Codigo/control/ManterSala.java
@@ -1,6 +1,5 @@
 package control;
 
-import java.sql.SQLException;
 import java.util.Vector;
 
 import persistence.SalaDAO;
@@ -9,7 +8,7 @@ import model.Sala;
 
 public class ManterSala {
 
-	private Vector<Sala> salas_vet;
+	private Vector<Sala> salas_vet = SalaDAO.getInstance().buscarTodos();
 	private static ManterSala instance;
 
 	private ManterSala() {
@@ -21,26 +20,24 @@ public class ManterSala {
 		return instance;
 	}
 	
-	public Vector<Sala> getSalas_vet() throws SQLException, PatrimonioException{
-		this.salas_vet = SalaDAO.getInstance().buscarTodos();
+	public Vector<Sala> getSalas_vet(){
 		return this.salas_vet;
 	}
 
-	public void inserir(String codigo, String descricao, String capacidade) throws PatrimonioException, SQLException {
+	public void inserir(String codigo, String descricao, String capacidade) throws PatrimonioException {
 		Sala sala = new Sala(codigo, descricao, capacidade);
 		SalaDAO.getInstance().incluir(sala);
 		this.salas_vet.add(sala);
 	}
 
-	public void alterar(String codigo, String descricao, String capacidade, Sala sala) throws PatrimonioException, SQLException {
-		Sala old_sala = sala;
+	public void alterar(String codigo, String descricao, String capacidade, Sala sala) throws PatrimonioException {
 		sala.setCodigo(codigo);
 		sala.setDescricao(descricao);
 		sala.setCapacidade(capacidade);
-		SalaDAO.getInstance().alterar(old_sala, sala);
+		SalaDAO.getInstance().alterar(sala);
 	}
 
-	public void excluir(Sala sala) throws SQLException {
+	public void excluir(Sala sala) {
 		SalaDAO.getInstance().excluir(sala);
 		this.salas_vet.remove(sala);
 	}
