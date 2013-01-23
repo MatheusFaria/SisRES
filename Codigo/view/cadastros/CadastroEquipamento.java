79980ed3fc2fea57947f3d3e863a35c076b9c564
aulusdiniz <aulus999@gmail.com>
2013-01-23 17:37:26 -0200

    Update Codigo/view/cadastros/CadastroEquipamento.java
    ainda dispara exception no final do processo.

--- Codigo/view/cadastros/CadastroEquipamento.java
+++ Codigo/view/cadastros/CadastroEquipamento.java
@@ -1,46 +1,32 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package view.cadastros;
 
-
-import control.ManterPatrimonio;
-import exception.PatrimonioException;
 import java.sql.SQLException;
+import exception.PatrimonioException;
 import javax.swing.JOptionPane;
-
+import control.ManterPatrimonio;
 /**
  * @author Parley
  * @editor Aulus
  */
 public class CadastroEquipamento extends CadastroPatrimonio {
-
-	public CadastroEquipamento(java.awt.Frame parent, boolean modal) {
-		super(parent, modal);
-		this.capacidadeLbl.setVisible(false);
-		this.capacidadeTxtField.setVisible(false);
-	}
-
-	@Override
-	protected void cadastroAction() {
-
-		try {
-			ManterPatrimonio.getInstance().inserir(codigoTxtField.getText(), descricaoTextArea.getText());
-			JOptionPane.showMessageDialog(this, "Equipamento Cadastrado com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE, null);
-			this.setVisible(false);
-
-
-		} catch (PatrimonioException ex) {
-			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-		} catch (SQLException ex) {
-			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-		} catch (NullPointerException ex) {
-			JOptionPane.showMessageDialog(this, ex.getLocalizedMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-		}
-
-
-
-
-	}
+    public CadastroEquipamento(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        this.capacidadeLbl.setVisible(false);
+	this.capacidadeTxtField.setVisible(false);
+	//A classe pai não pode conter elementos que a filha não contenha.
+	//Na classe de sala é que deve ser criado o componente extra.
+    }
+    
+    @Override
+	protected void cadastroAction() {	        
+   	try {
+            ManterPatrimonio.getInstance().inserir(codigoTxtField.getText(), descricaoTextArea.getText());
+            JOptionPane.showMessageDialog(this, "Equipamento Cadastrado com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE, null);
+            this.setVisible(false);
+        } catch (PatrimonioException ex) {
+		JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+        } catch (SQLException ex) {
+        	JOptionPane.showMessageDialog(this, ex.getSQLState() + "\n" + ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+        }
+    }
 }
