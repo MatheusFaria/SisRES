c64db925f7dbf14600fc5553394332d899e76e2d
aulusdiniz <aulus999@gmail.com>
2013-01-23 14:52:20 -0200

    Update Codigo/view/mainViews/EquipamentoView.java
    Problema com o componente codigoTxtField, não consegue encontrar e avisa que está em branco.

--- Codigo/view/mainViews/EquipamentoView.java
+++ Codigo/view/mainViews/EquipamentoView.java
@@ -1,92 +1,110 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package view.mainViews;
 
+import model.Patrimonio;
+import view.mainViews.EquipamentoView;
 import view.cadastros.CadastroEquipamento;
+import view.alteracoes.AlterarEquipamento;
 import exception.PatrimonioException;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.Vector;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputMethodEvent;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
-import model.Patrimonio;
 
 /**
  *
  * @author Parley
  */
 public class EquipamentoView extends PatrimonioView {// implements CadastroGeral{
-
+    
 	/**
-	 * Creates new form ClienteView
-	 */
+     * Creates new form EquipamentoView
+     */
 	public EquipamentoView(java.awt.Frame parent, boolean modal) {
 		super(parent, modal);
 		this.setTitle("Equipamentos");
-		pesquisarLbl.setText("Equipamento desejado: ");
+		//initComponents();
 	}
 
-	private Vector<String> fillDataVector(Patrimonio equipamento) {
+	private Vector <String> fillDataVector(Patrimonio equipamento) {
 
-		if (equipamento == null) {
+		if(equipamento == null)
 			return null;
-		}
-
-		Vector<String> nomesTabela = new Vector<String>();
+		
+		Vector <String> nomesTabela = new Vector<String> ();
 		nomesTabela.add(equipamento.getCodigo());
 		nomesTabela.add(equipamento.getDescricao());
-
 
 		return nomesTabela;
 
 	}
 
 	protected DefaultTableModel fillTable() { 
-		DefaultTableModel table = new DefaultTableModel();
-		/*
-		try {
-			DefaultTableModel table = new DefaultTableModel();
-
-			Iterator i = ManterPatrimonio.getInstance().getPatrimonios_vet().iterator();
-
-			table.addColumn("Codigo");
-			table.addColumn("Nome");
-			while (i.hasNext()) {
-				Patrimonio equipamento = (Patrimonio) i.next();
-				table.addRow(fillDataVector(equipamento));
-			}
-
-			return table;
-		} catch (PatrimonioException ex) {
-			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-		} catch (SQLException ex) {
-			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+        try {
+            DefaultTableModel table = new DefaultTableModel();
+            
+            Iterator<Patrimonio>  i = control.ManterPatrimonio.getInstance().getPatrimonio_vet().iterator();
+                        
+            table.addColumn("Codigo");     
+            table.addColumn("Descricao");
+            while(i.hasNext()) {
+                Patrimonio equipamento =  (Patrimonio) i.next();
+                table.addRow(fillDataVector(equipamento));
+            	}
+            return table;
+	        } catch (PatrimonioException ex) {
+	            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+	        	} catch (SQLException ex) {
+	            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+	        		}
+        return null;
 		}
-*/
-		return table;
 
-	}
-
+    private void cadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cadastrarActionPerformed
+        // TODO add your handling code here:
+    	cadastrarAction();
+    	}//GEN-LAST:event_cadastrarActionPerformed
+	
 	@Override
 	protected void cadastrarAction() {
 		CadastroEquipamento cadastro = new CadastroEquipamento(new javax.swing.JFrame(), true);
 		cadastro.setResizable(false);
 		cadastro.setVisible(true);
 		this.tabelaPatrimonio.setModel(fillTable());
-	}
+		}
 
+	private void alterarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_alterarActionPerformed
+		int index = this.tabelaPatrimonio.getSelectedRow();
+		alterarAction(index);
+		}
+	
 	@Override
 	protected void alterarAction(int index) {
 		/*
-		 * AlterarEquipamento alteracao = new AlterarEquipamento(new javax.swing.JFrame(), true, index);
+		 * AlterarEquipamento alteracao = new AlterarEq	uipamento(new javax.swing.JFrame(), true, index);
 		alteracao.setResizable(false);
 		alteracao.setVisible(true);
 		this.tabelaPatrimonio.setModel(fillTable());
 		*/
+        if(index < 0){
+            JOptionPane.showMessageDialog(this, "Selecione uma linha!", "Erro", JOptionPane.ERROR_MESSAGE, null);
+            return;
+        }
+
+        AlterarEquipamento alteracao = new AlterarEquipamento(new javax.swing.JFrame(), true, index);
+        alteracao.setResizable(false);
+        alteracao.setVisible(true);
+        this.tabelaPatrimonio.setModel(fillTable());
 	}
 
+    private void excluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_excluirActionPerformed
+        // TODO add your handling code here:
+		 int index = this.tabelaPatrimonio.getSelectedRow();
+		 excluirAction(index);
+    }//GEN-LAST:event_excluirActionPerformed
+	
 	@Override
 	protected void excluirAction(int index) {
 		/*
@@ -107,5 +125,181 @@ public class EquipamentoView extends PatrimonioView {// implements CadastroGeral
 		}
 		*/
 	}
+	
+    public void initComponents() {
 
+        panelBotoes = new javax.swing.JPanel();
+        cadastrar = new javax.swing.JButton();
+        alterar = new javax.swing.JButton();
+        excluir = new javax.swing.JButton();
+        visualizarBtn = new javax.swing.JButton();
+        panelLista = new javax.swing.JPanel();
+        pesquisarLbl = new javax.swing.JLabel();
+        pesquisarBtn = new javax.swing.JButton();
+        pesquisarTextField = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tabelaPatrimonio = new javax.swing.JTable();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Equipamentos");
+
+        panelBotoes.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        cadastrar.setText("Cadastrar");
+        cadastrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cadastrarActionPerformed(evt);
+            }
+        });
+
+        alterar.setText("Alterar");
+        //alterar.setEnabled(false);
+        alterar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                alterarActionPerformed(evt);
+            }
+        });
+
+        excluir.setText("Excluir");
+        //excluir.setEnabled(false);
+        excluir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                excluirActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout panelBotoesLayout = new javax.swing.GroupLayout(panelBotoes);
+        panelBotoes.setLayout(panelBotoesLayout);
+        panelBotoesLayout.setHorizontalGroup(
+            panelBotoesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelBotoesLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(panelBotoesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    //.addComponent(visualizarBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(excluir, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(alterar, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(cadastrar, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 135, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        panelBotoesLayout.setVerticalGroup(
+            panelBotoesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBotoesLayout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(cadastrar, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(alterar, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(excluir, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                //.addComponent(visualizarBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(29, Short.MAX_VALUE))
+        );
+
+        pesquisarLbl.setText("Equipamento desejado: "); 
+        tabelaPatrimonio.setModel(fillTable());
+        
+        pesquisarBtn.setText("Pesquisar");
+        pesquisarBtn.setEnabled(false);
+        pesquisarBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                pesquisarBtnActionPerformed(evt);
+            }
+
+			private void pesquisarBtnActionPerformed(ActionEvent evt) {
+				// TODO Auto-generated method stub
+				
+			}
+        });
+
+        pesquisarTextField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                pesquisarTextFieldActionPerformed(evt);
+            }
+
+			private void pesquisarTextFieldActionPerformed(ActionEvent evt) {
+				// TODO Auto-generated method stub
+				
+			}
+        });
+        pesquisarTextField.addInputMethodListener(new java.awt.event.InputMethodListener() {
+            public void caretPositionChanged(java.awt.event.InputMethodEvent evt) {
+            }
+            public void inputMethodTextChanged(java.awt.event.InputMethodEvent evt) {
+                pesquisarTextFieldInputMethodTextChanged(evt);
+            }
+			private void pesquisarTextFieldInputMethodTextChanged(InputMethodEvent evt) {
+				// TODO Auto-generated method stub
+				
+			}
+        });
+
+        javax.swing.GroupLayout panelListaLayout = new javax.swing.GroupLayout(panelLista);
+        panelLista.setLayout(panelListaLayout);
+        panelListaLayout.setHorizontalGroup(
+            panelListaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelListaLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(pesquisarLbl)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(pesquisarTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 209, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(pesquisarBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        panelListaLayout.setVerticalGroup(
+            panelListaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelListaLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(panelListaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(pesquisarLbl, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(pesquisarBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(pesquisarTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        tabelaPatrimonio.setRowSelectionAllowed(true);
+        jScrollPane1.setViewportView(tabelaPatrimonio);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(panelLista, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelBotoes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(panelLista, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(panelBotoes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    protected javax.swing.JButton alterar;
+    protected javax.swing.JButton cadastrar;
+    protected javax.swing.JButton excluir;
+    protected javax.swing.JScrollPane jScrollPane1;
+    protected javax.swing.JPanel panelBotoes;
+    protected javax.swing.JPanel panelLista;
+    protected javax.swing.JButton pesquisarBtn;
+    protected javax.swing.JLabel pesquisarLbl;
+    protected javax.swing.JTextField pesquisarTextField;
+    protected javax.swing.JTable tabelaPatrimonio;
+    protected javax.swing.JButton visualizarBtn;
+    // End of variables declaration//GEN-END:variables
 }
