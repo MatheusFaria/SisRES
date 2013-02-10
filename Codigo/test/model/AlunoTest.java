0a3f2e0b4dc954db4f106c9ab3f5e2c0f79513aa
Beatriz <beatriz.216@hotmail.com>
2013-02-10 15:58:02 -0200

    Testes da model Aluno

--- Codigo/test/model/AlunoTest.java
+++ Codigo/test/model/AlunoTest.java
@@ -6,7 +6,6 @@ import model.Aluno;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import exception.ClienteException;
@@ -25,32 +24,169 @@ public class AlunoTest {
 
 	@Test
 	public void testInstance() throws ClienteException {
-		Aluno p = new Aluno("Nome", "868.563.327-34", "123456", "1234-5678", "Nome@email");
-		assertTrue("Teste de Instanciamento do Aluno", p instanceof Aluno);
+		Aluno a = new Aluno("Nome", "040.757.021-70", "123456", "1234-5678", "Nome@email");
+		assertTrue("Teste de Instanciamento do Aluno", a instanceof Aluno);
+	}
+	
+	@Test
+	public void testNome() throws ClienteException {
+		Aluno a = new Aluno("Nome", "040.757.021-70", "123456", "1234-5678", "Nome@email");
+		assertTrue("Teste do Nome do Aluno", "Nome" == a.getNome());
+	}
+
+	@Test
+	public void testCpf() throws ClienteException {
+		Aluno a = new Aluno("Nome", "040.757.021-70", "123456", "1234-5678", "Nome@email");
+		assertTrue("Teste do CPF do Aluno", "040.757.021-70" == a.getCpf());
 	}
 	
 	@Test
 	public void testMatricula() throws ClienteException {
-		Aluno p = new Aluno("Nome", "868.563.327-34", "12/0034564", "1234-5678", "Nome@email");
-		assertTrue("Teste da Matricula do Aluno", "12/0034564" == p.getMatricula());
+		Aluno a = new Aluno("Nome", "040.757.021-70", "123456", "1234-5678", "Nome@email");
+		assertTrue("Teste da Matricula do Aluno", "123456" == a.getMatricula());
+	}
+	
+	@Test
+	public void testTelefone() throws ClienteException {
+		Aluno a = new Aluno("Nome", "040.757.021-70", "123456", "1234-5678", "Nome@email");
+		assertTrue("Teste de Telefone do Aluno", "1234-5678" == a.getTelefone());
+	}
+	
+	@Test
+	public void testEmail() throws ClienteException {
+		Aluno a = new Aluno("Nome", "040.757.021-70", "123456", "1234-5678", "Nome@email");
+		assertTrue("Teste do E-mail do Aluno", "Nome@email" == a.getEmail());
+	}
+
+	
+	
+	@Test (expected= ClienteException.class)
+	public void testNomeVazio() throws ClienteException {
+		new Aluno("", "040.757.02170", "123456", "1234-5678", "Nome@email");
+	}
+
+	@Test (expected= ClienteException.class)
+	public void testNomeNumero() throws ClienteException {
+		new Aluno("Nome12", "040.757.021-70", "123456", "1234-5678", "Nome@email");
+	}
+	
+	@Test (expected= ClienteException.class)
+	public void testNomeCaractere() throws ClienteException {
+		new Aluno("Nome*", "040.757.021-70", "123456", "1234-5678", "Nome@email");
+	}
+	
+	@Test (expected= ClienteException.class)
+	public void testNomeNulo() throws ClienteException {
+		new Aluno(null, "040.757.021-70", "123456", "1234-5678", "Nome@email");
+	}
+
+	
+	
+	@Test (expected= ClienteException.class)
+	public void testCpfVazio() throws ClienteException {
+		new Aluno("Nome", "", "123456", "1234-5678", "Nome@email");
+	}
+
+	@Test (expected= ClienteException.class)
+	public void testCpfLetras() throws ClienteException {
+		new Aluno("Nome", "O40.757.021-7O", "123456", "1234-5678", "Nome@email");
+	}
+	
+	@Test (expected= ClienteException.class)
+	public void testCpfDespadronizado() throws ClienteException {
+		new Aluno("Nome", "04075702170", "123456", "1234-5678", "Nome@email");
+	}
+	
+	@Test (expected = ClienteException.class)
+	public void testCpfInvalido() throws ClienteException {
+		new Aluno("Nome", "123.456.789-90", "123456", "1234-5678", "Nome@email");
+	}
+	
+	@Test (expected= ClienteException.class)
+	public void testCpfNulo() throws ClienteException {
+		new Aluno("Nome", null, "123456", "1234-5678", "Nome@email");
 	}
 	
 	
 	
 	@Test (expected= ClienteException.class)
 	public void testMatriculaVazia() throws ClienteException {
-		new Aluno("Nome", "868.563.327-34", "", "1234-5678", "Nome@email");
-	}
-	
-	@Ignore// (expected= ClienteException.class)
-	public void testMatriculaDespadronizada() throws ClienteException {
-		new Aluno("Nome", "868.563.327-34", "120034564", "1234-5678", "Nome@email");
+		new Aluno("Nome", "040.757.021-70", "", "1234-5678", "Nome@email");
 	}
 	
 	@Test (expected= ClienteException.class)
 	public void testMatriculaNula() throws ClienteException {
-		new Aluno("Nome", "868.563.327-34", null, "1234-5678", "Nome@email");
+		new Aluno("Nome", "040.757.021-70", null, "1234-5678", "Nome@email");
+	}
+	
+	
+	
+	@Test
+	public void testTelefoneVazio() throws ClienteException {
+		Aluno a = new Aluno("Nome", "040.757.021-70", "123456", "", "Nome@email");
+		assertTrue("Teste de Telefone Vazio do Aluno", "" == a.getTelefone());
+	}
+	
+	@Test (expected= ClienteException.class)
+	public void testTelefoneDespadronizado() throws ClienteException {
+		new Aluno("Nome", "040.757.021-70", "123456", "6133333333", "Nome@email");
+	}
+	
+	@Test (expected= ClienteException.class)
+	public void testTelefoneNulo() throws ClienteException {
+		new Aluno("Nome", "040.757.021-70", "123456", null, "Nome@email");
 	}
 
+	
+	
+	@Test
+	public void testEmailVazio() throws ClienteException {
+		Aluno a = new Aluno("Nome", "040.757.021-70", "123456", "1234-5678", "");
+		assertTrue("Teste de Email Vazio do Aluno", "" == a.getEmail());
+	}
+	
+	@Test (expected= ClienteException.class)
+	public void testEmailNulo() throws ClienteException {
+		new Aluno("Nome", "040.757.021-70", "123456", "9999-9999", null);
+	}
 
+	
+	
+	@Test
+	public void testEqualsTrue() throws ClienteException {
+		Aluno a = new Aluno("Nome", "040.757.021-70", "123456", "", "Nome@email");
+		Aluno ae = new Aluno("Nome", "040.757.021-70", "123456", "", "Nome@email");
+		assertTrue("Teste do E-mail do Aluno", a.equals(ae));
+	}
+	
+	@Test
+	public void testEqualsFalseNome() throws ClienteException {
+		Aluno a = new Aluno("Nome", "040.757.021-70", "123456", "", "Nome@email");
+		Aluno oa = new Aluno("NomeDiferente", "040.757.021-70", "12356", "(90) 9999-9999", "Nom@email");
+		assertFalse("Teste do E-mail do Aluno", a.equals(oa));
+	}
+	@Test
+	public void testEqualsFalseCpf() throws ClienteException {
+		Aluno a = new Aluno("Nome", "040.757.021-70", "123456", "", "Nome@email");
+		Aluno oa = new Aluno("Nome", "490.491.781-20", "12356", "(90) 9999-9999", "Nom@email");
+		assertFalse("Teste do E-mail do Aluno", a.equals(oa));
+	}
+	@Test
+	public void testEqualsFalseMatricula() throws ClienteException {
+		Aluno a = new Aluno("Nome", "040.757.021-70", "123456", "", "Nome@email");
+		Aluno oa = new Aluno("Nome", "040.757.021-70", "12356", "(90) 9999-9999", "Nom@email");
+		assertFalse("Teste do E-mail do Aluno", a.equals(oa));
+	}
+	@Test
+	public void testEqualsFalseTelefone() throws ClienteException {
+		Aluno a = new Aluno("Nome", "040.757.021-70", "123456", "", "Nome@email");
+		Aluno oa = new Aluno("Nome", "040.757.021-70", "123456", "(90) 9999-9999", "Nom@email");
+		assertFalse("Teste do E-mail do Aluno", a.equals(oa));
+	}
+	@Test
+	public void testEqualsFalseEmail() throws ClienteException {
+		Aluno a = new Aluno("Nome", "040.757.021-70", "123456", "", "Nome@email");
+		Aluno oa = new Aluno("Nome", "040.757.021-70", "123456", "", "Nom@el");
+		assertFalse("Teste do E-mail do Alunor", a.equals(oa));
+	}
 }
