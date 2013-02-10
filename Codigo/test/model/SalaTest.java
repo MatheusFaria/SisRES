bde36a64a231b9608980d3fa28382f362455baf8
aulusdiniz <aulus999@gmail.com>
2013-02-10 14:08:01 -0200

    Update Codigo/test/model/SalaTest.java

--- Codigo/test/model/SalaTest.java
+++ Codigo/test/model/SalaTest.java
@@ -36,6 +36,15 @@ public class SalaTest {
 	}
 	
 	@Test
+	public void testNotEqualsCapacidade() throws PatrimonioException {
+		setUp();
+		Sala sala_new = new Sala("codigo", "descricao", "2");
+		assertFalse("Falha no Equals.", sala.equals(sala_new));
+		sala_new = null;
+		tearDown();
+	}
+	
+	@Test
 	public void testNotEqualsDescricao() throws PatrimonioException {
 		setUp();
 		Sala sala_new = new Sala("codigo", "d", "1");
