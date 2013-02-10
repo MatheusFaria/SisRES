b11ac8568be92351dde9207ae6c60eca2f83d642
aulusdiniz <aulus999@gmail.com>
2013-02-10 20:05:41 -0200

    Update Codigo/test/control/ManterSalaTest.java

--- Codigo/test/control/ManterSalaTest.java
+++ Codigo/test/control/ManterSalaTest.java
@@ -6,27 +6,56 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
+
+import persistence.FactoryConnection;
 import control.ManterSala;
 import model.Sala;
 import exception.PatrimonioException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.Vector;
 
 
 public class ManterSalaTest {
 
-  private static Sala sala;
+	private static Sala sala;
 	private static Sala sala_new;
+	Vector<Sala> sala_vet;
 	
 	@Before
 	public void setUp() throws SQLException, PatrimonioException{
-		sala = new Sala("codigo", "descricao", "66");
-		sala_new = new Sala("novo_codigo","nova_descricao","88");
+		sala = new Sala("codigo_old", "descricao", "1");
+		sala_new = new Sala("codigo", "descricao", "2");
+	}
+	
+	@Before
+	public void setUpIncluir() throws SQLException, PatrimonioException{
+		sala = new Sala("codigo_old", "descricao", "1");
+		sala_new = new Sala("codigo", "descricao", "2");
+		
+		this.executaNoBanco("INSERT INTO " +
+				"sala (codigo, descricao, capacidade) VALUES (" +
+				"\"" + sala.getCodigo() + "\", " +
+				"\"" + sala.getDescricao() + "\", " +
+				"\"" + sala.getCapacidade() + "\"); "
+				);
 	}
 	
 	@After
 	public void tearDown() throws SQLException, PatrimonioException{
-		ManterSala.getInstance().excluir(sala);
-		ManterSala.getInstance().excluir(sala_new);
+		this.executaNoBanco("DELETE FROM sala WHERE " +
+				"sala.codigo = \"" + sala_new.getCodigo() + "\" and " +
+				"sala.descricao = \"" + sala_new.getDescricao() +  "\" and " +
+				"sala.capacidade = \"" + sala_new.getCapacidade() + "\";"
+				);
+		this.executaNoBanco("DELETE FROM sala WHERE " +
+				"sala.codigo = \"" + sala.getCodigo() + "\" and " +
+				"sala.descricao = \"" + sala.getDescricao() +  "\" and " +
+				"sala.capacidade = \"" + sala.getCapacidade() + "\";"
+				);				
 		
 	}
 	
@@ -52,27 +81,44 @@ public class ManterSalaTest {
 	@Test
 	public void testInserir() throws PatrimonioException, SQLException {
 		setUp();
-		ManterSala.getInstance().inserir("codigo", "descricao", "66");
-		assertNotNull("Falha ao inserir", ManterSala.getInstance().procurarNoVetor(sala));
+		ManterSala.getInstance().inserir("codigo", "descricao", "2");
+		assertNotNull("Falha ao inserir", this.procurarNoVetor(sala_new));
 		tearDown();
 	}
 
-	@Test
+	/*@Test
 	public void testAlterar() throws PatrimonioException, SQLException {
-		setUp();
-		ManterSala.getInstance().inserir("codigo", "descricao", "66");
-		ManterSala.getInstance().alterar("codigo", "descricao", "88", sala);
-		assertNotNull("Falha ao alterar", ManterSala.getInstance().procurarNoVetor(sala));
+		setUpIncluir();
+		ManterSala.getInstance().alterar("codigo_old", "descricao", "1", sala_new);
+		assertNotNull("Falha ao alterar", this.procurarNoVetor(sala_new));
 		tearDown();
-	}
+	}*/
 
 	@Test
 	public void testExcluir() throws SQLException, PatrimonioException {
-		setUp();
-		ManterSala.getInstance().inserir("codigo", "descricao", "66");
+		setUpIncluir();
 		ManterSala.getInstance().excluir(sala);
-		assertNull("Falha ao excluir", ManterSala.getInstance().procurarNoVetor(sala));
+		assertNull("Falha ao excluir", this.procurarNoVetor(sala));
 		tearDown();
 	}
 
+	public Sala procurarNoVetor(Sala teste) throws PatrimonioException, SQLException {
+		Vector<Sala> todos = ManterSala.getInstance().getSalas_vet();
+		Iterator<Sala> i = todos.iterator();
+		while(i.hasNext()){
+			Sala e = i.next();
+			if(e.equals(teste))
+				return e;			
+		}
+		return null;
+	}
+	
+	private void executaNoBanco(String msg) throws SQLException{
+		Connection con =  FactoryConnection.getInstance().getConnection();
+		PreparedStatement pst = con.prepareStatement(msg);
+		pst.executeUpdate();
+		pst.close();
+		con.close();
+	}
+	
 }
