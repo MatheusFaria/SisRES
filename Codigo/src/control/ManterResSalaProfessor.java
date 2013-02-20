35354fd3111ae1ae242f24963dae97a3ebd08b6e
Matheus Faria <matheus.sousa.faria@gmail.com>
2013-02-20 12:09:01 -0300

    Repondo Arquivos q o Parley Apagou

--- Codigo/src/control/ManterResSalaProfessor.java
+++ Codigo/src/control/ManterResSalaProfessor.java
@@ -3,6 +3,8 @@ package control;
 import java.sql.SQLException;
 import java.util.Vector;
 
+import persistence.ResSalaProfessorDAO;
+
 import model.Professor;
 import model.ReservaSalaProfessor;
 import model.Sala;
@@ -24,28 +26,45 @@ public class ManterResSalaProfessor {
 	}
 	//
 		
-	public Vector<Object> getResProfessorSala_vet() {
-		return null;
+	public Vector<Object> getResProfessorSala_vet() throws SQLException, ClienteException, PatrimonioException, ReservaException {
+		this.rev_sala_professor_vet = ResSalaProfessorDAO.getInstance().buscarTodos();
+		return this.rev_sala_professor_vet;
 	}
 
-	public void inserir() throws PatrimonioException, SQLException, ClienteException, ReservaException {
-		//TODO Com todos os parametros como String.
+	public void inserir(String codigo, String descricao, String capacidade, 
+			String nome, String cpf, String matricula, String telefone, String email,
+			String data, String hora, String finalidade) 
+				throws PatrimonioException, SQLException, ClienteException, ReservaException {
+		
+		Sala sala = new Sala(codigo, descricao, capacidade);
+		Professor professor = new Professor(nome, cpf, matricula, telefone, email);
+		ReservaSalaProfessor reserva = new ReservaSalaProfessor(data, hora, sala, finalidade, professor);
+		ResSalaProfessorDAO.getInstance().incluir(reserva);
+		this.rev_sala_professor_vet.add(reserva);
 	}
 	
 	public void inserir(Sala sala, Professor prof,
 						String data, String hora, String finalidade, String cadeiras_reservadas) 
 					throws SQLException, ReservaException {
 
-		//TODO Falta o corpo deste metodo
+		ReservaSalaProfessor reserva = new ReservaSalaProfessor(data, hora, sala , finalidade, prof);
+		ResSalaProfessorDAO.getInstance().incluir(reserva);
+		this.rev_sala_professor_vet.add(reserva);
 	}
 
-	public void alterar(String finalidade, ReservaSalaProfessor r) 
+	public void alterar(String finalidade, ReservaSalaProfessor reserva) 
 				throws SQLException, ReservaException {
-		//TODO Falta o corpo deste metodo
+		
+		ReservaSalaProfessor reserva_old = new ReservaSalaProfessor(reserva.getData(), reserva.getHora(), reserva.getSala() , 
+				reserva.getFinalidade(), reserva.getProfessor());
+		
+		reserva.setFinalidade(finalidade);
+		ResSalaProfessorDAO.getInstance().alterar(reserva_old, reserva);
 		
 	}
 
 	public void excluir(ReservaSalaProfessor r) throws SQLException, ReservaException {
-		//TODO Falta o corpo deste metodo
+		ResSalaProfessorDAO.getInstance().excluir(r);
+		this.rev_sala_professor_vet.remove(r);
 	}
 }
